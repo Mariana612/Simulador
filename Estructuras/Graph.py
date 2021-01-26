8b19c1dc1bb444b8e8399b2d1abe9562d44cabfe
Ignacio <ignaevc@hotmail.com>
2021-01-26 14:53:10 -0600

    Correcciones pequeñas y gitignore de pycache agreagdos

--- Estructuras/Graph.py
+++ Estructuras/Graph.py
@@ -1,24 +1,30 @@
 from Estructuras import Node
-class Grafo:
+
+
+class Graph:
     def __init__(self):
-        self._Nodes=[]
-    def searchnode(self,Name):
-        #Busqueda del nodo mediante el nombre
-        for i in range(0,len(self._Nodes)):
-            if self._Nodes[i].name==Name:
+        self._Nodes = []
+
+    def searchnode(self, Name):
+        # Busqueda del nodo mediante el nombre
+        for i in range(0, len(self._Nodes)):
+            if self._Nodes[i].name == Name:
                 return self._Nodes[i]
             else:
                 continue
-    def addnode(self,name,value):
-        #Creación del nodo
-        NewNode=Node.Node(name,value)
-        #Adición a la lista de nodos
+
+    def addnode(self, name, value):
+        # Creación del nodo
+        NewNode = Node.Node(name, value)
+        # Adición a la lista de nodos
         self._Nodes.append(NewNode)
-    def makeconection(self,Name1,Name2):
-        #Registrar conexión en el primer nodo
+
+    def makeconection(self, Name1, Name2):
+        # Registrar conexión en el primer nodo
         self.searchnode(Name1).addconnection(self.searchnode(Name2))
-        #Registrar conexión en el segundo nodo
+        # Registrar conexión en el segundo nodo
         self.searchnode(Name2).addconnection(self.searchnode(Name1))
-    def searchshortestpath(self,Name1,Name2):
-        Node1=self.searchnode(Name1)
-        Node2=self.searchnode(Name2)
+
+    def searchshortestpath(self, Name1, Name2):
+        Node1 = self.searchnode(Name1)
+        Node2 = self.searchnode(Name2)
--- Estructuras/Node.py
+++ Estructuras/Node.py
@@ -1,10 +1,11 @@
 class Node:
-    def __init__(self,name,value):
-        self.name=name
-        self.value=value
-        self.Conections={}
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.Conections = {}
 
-    def addconnection(self,Nodo):
-        self.Conections[Nodo.name]=0
-    def setpeso(self,Node,peso):
-        self.Conections[Node.name]=peso
+    def addconnection(self, Node):
+        self.Conections[Node.name] = 0
+
+    def setpeso(self, Node, peso):
+        self.Conections[Node.name] = peso
