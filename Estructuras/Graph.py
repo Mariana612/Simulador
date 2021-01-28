ec7496628a9edf86bc1d923a75cf3b5a7f5eabef
Marcelo Truque <machotm@hotmail.com>
2021-01-28 14:16:40 -0600

    Fusion de Nodos Eliminación de conexión Conexiones múltiples entre dos nodos

--- Estructuras/Graph.py
+++ Estructuras/Graph.py
@@ -19,14 +19,32 @@ class Graph:
         # Adición a la lista de nodos
         self._Nodes.append(NewNode)
         return NewNode
-
+    def fusenodes(self,Name1,Name2):
+        Node1=self.searchnode(Name1)
+        Node2=self.searchnode(Name2)
+        Node1.Points+=Node2.Points
+        Dictionary=Node2.Conections.copy()
+        Keys=Dictionary.keys()
+        for i in Keys:
+            if i==Node1.name:
+                Node1.eliminateConnection(Node1,Node2)
+            elif i in Node1.Conections:
+                Fuse=self.searchnode(i)
+                Node1.Conections[i]=[Node1.Conections[i],Node2.Conections[i]]
+                self.QuickSort(Node1.Conections[i])
+                Fuse.Conections[Node1.name]=Node1.Conections[i]
+                Node2.eliminateConnection(Node2,self.searchnode(i))
+            else:
+                self.makeconection(Node1.name,i,Node2.Conections[i])
+                Node2.eliminateConnecion(Node2,self.searchnode(i))
+        self._Nodes.remove(Node2)
+        Node2.Points=[]
 
     def makeconection(self, Name1, Name2, peso):
         # Registrar conexión en el primer nodo
         self.searchnode(Name1).addconnection(self.searchnode(Name2),peso)
         # Registrar conexión en el segundo nodo
         self.searchnode(Name2).addconnection(self.searchnode(Name1),peso)
-
     def searchshortestpath(self, Name1, Name2):
         Node1 = self.searchnode(Name1)
         Node2 = self.searchnode(Name2)
@@ -62,3 +80,14 @@ class Graph:
         Lista[index+1],Lista[High]=Lista[High],Lista[index+1]
         return i+1
 
+Grafo=Graph()
+Grafo.addnode("Mike",12)
+Grafo.addnode("Kory",90)
+Grafo.addnode("Banana",34)
+Grafo.makeconection("Mike","Kory",23)
+Grafo.makeconection("Kory","Banana",90)
+Grafo.makeconection("Banana","Mike",7)
+Grafo.searchnode("Banana").Points=[1,2,3,4]
+Grafo.searchnode("Mike").Points=[4,5,6,7]
+Grafo.fusenodes("Banana","Mike")
+print("Fin")
--- Estructuras/Node.py
+++ Estructuras/Node.py
@@ -4,6 +4,11 @@ class Node:
         self.value = value
         self.Conections = {}
         self.Points = []
-
+    def eliminateConnection(self,Node1,Node2):
+        Node1.Conections.pop(Node2.name)
+        Node2.Conections.pop(Node1.name)
     def addconnection(self, Node,Peso) :
-        self.Conections[Node.name] = Peso
+        if Node.name in self.Conections:
+            self.Conections[Node.name]=[self.Conections[Node.name],Peso]
+        else:
+            self.Conections[Node.name]=Peso
